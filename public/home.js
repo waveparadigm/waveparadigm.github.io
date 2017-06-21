c77163528395e624cc77bde6918c86a665eca57c
Adam Gincel <agincel@stevens.edu>
2017-06-20 22:47:08 -0400

    updated Bean gif and added Rig 'er Bigger gif

--- public/home.js
+++ public/home.js
@@ -2,11 +2,12 @@ var myGfys = [
 	"https://gfycat.com/ifr/WetHoarseAnnelid", //Spherical Saunter M
 	"https://gfycat.com/ifr/UnevenHairyBorzoi", //We 2 Guys (needs update)
 	"https://gfycat.com/ifr/TerrificDopeyFlyingfish", //Pause
-	"https://gfycat.com/ifr/ColorfulKindheartedBeardedcollie", //Adventures of Bean (needs update)
+	"https://gfycat.com/ifr/ElaborateKindlyGoat", //Adventures of Bean
 	"https://gfycat.com/ifr/ConstantUltimateIchthyosaurs", //Tracks
 	"https://gfycat.com/ifr/ImportantTatteredAgama", //Meltdown
 	"https://gfycat.com/ifr/IdioticDiligentDassie", //Big Palladium
 	"https://gfycat.com/ifr/DismalFamiliarAlaskankleekai", //VRange
+	"https://gfycat.com/ifr/MarvelousSmallDowitcher", //Rig 'er Bigger
 ];
 
 var a = -1;
