88feaaf9a71eb6bcee5c99e33217b77883697723
Adam Gincel <agincel@stevens.edu>
2020-07-04 03:23:24 -0400

    complete site overhaul

--- public/home.js
+++ public/home.js
@@ -1,80 +1,68 @@
-let myVideos = [
-	["./videos/memory.webm", "Memory"],
-	["./videos/tracks.webm", "Tracks"],
-	["./videos/meltdown.webm", "Meltdown"],
-	["./videos/pause.webm", "Pause"],
-	["./videos/sphericalsaunterm.webm", "Spherical Saunter M"],
-	["./videos/windslime.webm", "Wind Slime"],
-	["./videos/engineerarena.webm", "Engineer Arena"]
-];
-
-var games = [
+let games = [
 	{
 		title: "Memory",
-		description: "Live through the memorysort experiment and explore three different platforming mechanics.",
-		image: "img/games/memory.png",
+		shortname: "memory",
+		date: "Spring 2016",
+		video: "./videos/memory.webm",
+		description: "Live through the memorysort experiment and explore three different platforming mechanics.<br /><br />This First Person Puzzle/Platforming game was the culmination and combination of three different earlier projects of mine - Ethereal Escape, Impulse, and Pause - and combined their mechanics in fun and interesting ways. It was the winner of the Stevens Game Development Club's \"Game Jam VII\"!",
 		links: [["https://waveparadigm.itch.io/memory", "itch.io"]]
 	},
 	{
 		title: "Tracks",
-		description: "Solve moral quandaries in this on-rails experience.",
-		image: "img/games/tracks.png",
+		shortname: "tracks",
+		date: "Fall 2016",
+		video: "./videos/tracks.webm",
+		description: "Solve moral quandaries in this on-rails experience.<br /><br />Tracks was built in GameMaker: Studio and saw a surprising surge in online plays after its release - around 5000 or so!",
 		links: [["https://waveparadigm.itch.io/tracks", "itch.io"], ["http://adamgincel.com/tracks", "Play Online"]]
 	},
 	{
 		title: "Meltdown",
-		description: "Experience the intricacies of Nuclear Reactor operation. Made in conjunction with James Romph (<a href=\"http://tehpilot.com\">website</a>) and Professor Alex Wellerstein, Meltdown aims to teach a realistic story about the events that lead up to nuclear reactor meltdown. Fall 2016.",
-		image: "img/games/meltdown.png",
+		shortname: "meltdown",
+		date: "Spring 2017",
+		video: "./videos/meltdown.webm",
+		description: "Experience the intricacies of Nuclear Reactor operation. Made in conjunction with James Romph (<a href=\"http://pilot.dev\">website</a>) and Professor Alex Wellerstein, Meltdown aims to teach a realistic story about the events that lead up to nuclear reactor meltdown.",
 		links: [["https://waveparadigm.itch.io/meltdown", "itch.io"]]
 	},
 	{
 		title: "Pause",
-		description: "Pause and Unpause the world! Freeze time to guide object(s) to the end of levels with many moving pieces. 2016-2018.",
-		image: "img/games/pause.png",
+		shortname: "pause",
+		date: "2016 - 2018",
+		video: "./videos/pause.webm",
+		description: "Pause and Unpause the world! Freeze time to guide object(s) to the end of levels with many moving pieces.<br /><br />Pause first saw life as a First Person Puzzle game, but eventually translated to a one-tap mobile experience. It's been put on the backburner for now, but I hope to one day return to finish it!",
 		links: [["https://waveparadigm.itch.io/pause", "itch.io"], ["http://adamgincel.com/pause", "Play Online"], ["https://play.google.com/apps/testing/com.WaveParadigm.Pause17", "Google Play"]]
 	},
 	{
 		title: "Tilt 'em",
-		description: "Inspired by rolling legends before it, tilt the world and possess different objects in this arcade platforming experience!",
-		image: "./img/games/sphericalsaunterm.png",
+		shortname: "tiltem",
+		date: "2017 - 2019",
+		video: "./videos/sphericalsaunterm.webm",
+		description: "Inspired by rolling legends before it, tilt the world and possess different objects in this arcade platforming experience!<br /><br />Tilt 'em first saw life as \"Spherical Saunter R\", a complete rebuild and enhancement of the original Spherical Saunter. Once I learned how create that stage tilt effect, I branched the project off into \"Spherical Saunter M\". Finally, once possessing many objects was added as a mechanic, the game was re-branded to \"Tilt 'em\". Unfortunately, it is another project which has been placed on the backburner in favor of newer endeavors.",
 		links: [["https://waveparadigm.itch.io/spherical-saunter-m", "itch.io"]]
 	},
 	{
 		title: "Wind Slime",
-		description: "The roaming spirit of a once-great wind mage finds a home in a small, unassuming mountain slime. He tasks himself with climbing to the top of the mountain, harnessing the power of the mountain's mana crystals to use a small fraction of his former magic, manipulating the endless blizzard to guide him up the hazardous slope.",
-		image: "./img/games/windslime.png",
+		shortname: "windslime",
+		date: "Fall 2018",
+		video: "./videos/windslime.webm",
+		description: "The roaming spirit of a once-great wind mage finds a home in a small, unassuming mountain slime. He tasks himself with climbing to the top of the mountain, harnessing the power of the mountain's mana crystals to use a small fraction of his former magic, manipulating the endless blizzard to guide him up the hazardous slope.<br/ ><br />Wind Slime was created for the 2018 Kenney Game Jam, and was a top contender!",
 		links: [["https://waveparadigm.itch.io/wind-slime", "itch.io"]]
 	},
 	{
 		title: "Engineer Arena",
-		description: "A frantic local-multiplayer splitscreen FPS where you upgrade on the fly! Take on your friends in the arena and come out on top.",
-		image: "./img/games/engineerarena.png",
+		shortname: "engineerarena",
+		date: "2018 - 2019",
+		video: "./videos/engineerarena.webm",
+		description: "A frantic local-multiplayer splitscreen FPS where you upgrade on the fly! Take on your friends in the arena and come out on top.<br /><br />Engineer Arena was built with a team of 7 people over a year of development time, and was the first game I released on Steam!",
 		links: [["https://store.steampowered.com/app/1118950/Engineer_Arena/", "Steam"], ["https://waveparadigm.itch.io/engineer-arena/", "itch.io"],
-				["https://waveparadigm.dev/engineerarena/", "Site"], ["https://twitter.com/EngineerArena", "Twitter"], ["https://waveparadigm.dev/engineerarena/presskit/", "PressKit()"]]
-	}
-];
-
-
-let social = [
-	{
-		img: "./img/twitter.png",
-		link: "https://twitter.com/waveparadigm"
+				["https://waveparadigm.com/engineerarena/", "Site"], ["https://twitter.com/EngineerArena", "Twitter"], ["https://waveparadigm.com/engineerarena/presskit/", "PressKit()"]]
 	},
 	{
-		img: "./img/itchio.png",
-		link: "https://waveparadigm.itch.io"
-	},
-	{
-		img: "./img/bandcamp.png",
-		link: "https://waveparadigm.bandcamp.com"
-	},
-	{
-		img: "./img/github.png",
-		link: "https://github.com/waveparadigm"
-	},
-	{
-		img: "./img/youtube.png",
-		link: "https://www.youtube.com/channel/UChEo7pMS9CTI3vCibtoagOA"		
+		title: "Slime 64",
+		shortname: "slime64",
+		date: "2019 - Present",
+		video: "./videos/slime64.webm",
+		description: "A 3D collectathon platformer inspired by 64-bit classics. Journey around the world as members of the four Slime societies: Earth, Air, Water, and Fire. Help them come together to save their world, while collecting everything they possibly can along the way.<br /><br />Slime 64 is still in active development (with a team of, at one point, 12 people!), and is on pace to be the most expansive project I've ever worked on. I'm really excited for its continued progress - follow us on Twitter!",
+		links: [["https://twitter.com/Slime_64", "Twitter"]]
 	}
 ];
 
@@ -85,22 +73,9 @@ let vid = null;
 
 window.onload = function() {
 	vid = document.getElementById("vid");
-	a = myVideos.length - 2;
-	
-	fillSocial();
-	fillGifIndicator();
-	nextGfy();
-}
-
-function prevGfy() {
-	a -= 1;
-	if (a < 0)
-		a = myVideos.length - 1;
-	vid.src = myVideos[a][0];
-	vid.load();
-	vid.play();
-
-	updateIndicators();
+	if (window.location.hash) {
+		this.setGame(window.location.hash.substr(1));
+	}
 }
 
 function nextGfy() {
@@ -114,79 +89,50 @@ function nextGfy() {
 	updateIndicators();
 }
 
-function fillBlog() {
-	var blog = document.getElementById("blog");
-	var htmlString = "";
-	for (var i = 0; i < blogEntries.length; i++) {
-		htmlString += "<li>\n<div class=\"date\">" + blogEntries[i][0] + "</div> " + blogEntries[i][1] + "\n</li>\n";
+function setVideo(src) {
+	if (src) {
+		vid.src = src;
+		vid.load();
+		vid.play();
 	}
-	blog.innerHTML = htmlString;
-}
-
-function fillSocial() {
-	let s = document.getElementById("social");
-	let htmlString = "";
-	for (let i = 0; i < social.length; i++) {
-		htmlString += "<a href=\"" + social[i].link + "\"><img src=\"" + social[i].img + "\" /></a>";
-	}
-	s.innerHTML = htmlString;
-}
-
-function fillGifIndicator() {
-	var gifIndicator = document.getElementById("gifIndicator");
-
-	var htmlString = "";
-	for (var i = 0; i < myVideos.length; i++) {
-		htmlString += "<div class=\"indicator";
-
-		if (i == a) {
-			htmlString += " active";
-		}
-
-		htmlString += "\" id=\"indicator" + i.toString() + "\"></div>";
-	}
-
-	gifIndicator.innerHTML = htmlString;
-}
-
-function updateIndicators() {
-	for (var i = 0; i < myVideos.length; i++) {
-		var ind = document.getElementById("indicator" + i.toString());
-		ind.classList.remove("active");
-
-		if (i == a) 
-			ind.classList.add("active");
-	}
-
-	document.getElementById("gifTitle").innerHTML = myVideos[a][1];
 }
 
 function populateGames() {
 	let s = "";
 	for (let i = 0; i < games.length; i++) {
 		let game = games[i];
-		s += `
-		<article class="game">
-			<div class="imagewrapper">
-				<img src="${game.image}" />
-			</div>
-			<section>
-				<h2>${game.title}</h2>
-				<p>${game.description}</p>
-
-				${formatLinks(game.links)}
-			</section>
-		</article>
-		`
+		s += `<a href="#${game.shortname}" onclick="setGame('${game.shortname}')"><li>${game.title}</li></a>
+		`;
 	}
-	document.getElementById("games").innerHTML = s;
+	document.getElementById("gamesOL").innerHTML = s;
 }
 
 function formatLinks(links) {
 	let s = "";
 	for (let i = 0; i < links.length; i++) {
-		s += `<a href="${links[i][0]}">${links[i][1]}</a>
+		s += `<li><a href="${links[i][0]}">${links[i][1]}</a></li>
 		`;
 	}
 	return s;
+}
+
+function setGame(shortname) {
+	let game = null;
+	for (let i = 0; i < games.length; i++) {
+		if (games[i].shortname == shortname) {
+			game = games[i];
+		}
+	}
+
+	if (!game) {
+		return;
+	}
+
+	document.getElementById("mainTitle").innerHTML = game.title;
+	document.getElementById("mainDate").innerHTML = game.date;
+
+	setVideo(game.video);
+
+	document.getElementById("mainLinks").innerHTML = formatLinks(game.links);
+	document.getElementById("mainDescription").innerHTML = game.description;
 }
