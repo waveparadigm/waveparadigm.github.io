8ea62eceacd73ce60766a52b6717102ed7117664
Adam Gincel <adam.gincel@gmail.com>
2023-10-10 22:57:53 -0400

    added slime64-feedback redirect

--- public/redirects.js
+++ public/redirects.js
@@ -2,6 +2,7 @@ function getRedirects()
 {
 	return [
         {name: "slime64-discord", link: "https://discord.com/invite/RZNyGTV8TF"},
-        {name: "slime64-steam", link: "https://slime64.com"}
+        {name: "slime64-steam", link: "https://slime64.com"},
+        {name: "slime64-feedback", link: "https://docs.google.com/forms/d/e/1FAIpQLSfJcnl0Gx4i5RqIk1g-LfgurlCI-DFaKy2MC7XTwYe6pBrF2Q/viewform"}
     ]
 }
