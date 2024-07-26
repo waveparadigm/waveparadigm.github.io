cd3f42a1c106f2536b885005c01480d9ebc2440a
Adam Gincel <adam.gincel@gmail.com>
2024-07-25 21:29:17 -0400

    added more redirects

--- public/redirects.js
+++ public/redirects.js
@@ -3,6 +3,19 @@ function getRedirects()
 	return [
         {name: "slime64-discord", link: "https://discord.com/invite/RZNyGTV8TF"},
         {name: "slime64-steam", link: "https://slime64.com"},
-        {name: "slime64-feedback", link: "https://docs.google.com/forms/d/e/1FAIpQLSfJcnl0Gx4i5RqIk1g-LfgurlCI-DFaKy2MC7XTwYe6pBrF2Q/viewform"}
+        {name: "slime64-feedback", link: "https://docs.google.com/forms/d/e/1FAIpQLSfJcnl0Gx4i5RqIk1g-LfgurlCI-DFaKy2MC7XTwYe6pBrF2Q/viewform"},
+        {name: "youtube", link: "https://www.youtube.com/channel/UChEo7pMS9CTI3vCibtoagOA"},
+        {name: "steam", link: "https://store.steampowered.com/search/?developer=WaveParadigm"},
+        {name: "itch", link: "https://waveparadigm.itch.io/"},
+        {name: "itchio", link: "https://waveparadigm.itch.io/"},
+        {name: "twitter", link: "https://twitter.com/waveparadigm"},
+        {name: "x", link: "https://twitter.com/waveparadigm"},
+        {name: "bandcamp", link: "https://waveparadigm.bandcamp.com/"},
+        {name: "music", link: "https://waveparadigm.bandcamp.com/"},
+        {name: "slime64-music", link: "https://waveparadigm.bandcamp.com/album/slime-64-ost"},
+        {name: "slime64-ost", link: "https://waveparadigm.bandcamp.com/album/slime-64-ost"},
+        {name: "slime64-soundtrack", link: "https://waveparadigm.bandcamp.com/album/slime-64-ost"},
+        {name: "core", link: "https://www.coregames.com/user/f261f4bb05b44bb2bf465b8a8346491f"},
+        {name: "coregames", link: "https://www.coregames.com/user/f261f4bb05b44bb2bf465b8a8346491f"}
     ]
 }
